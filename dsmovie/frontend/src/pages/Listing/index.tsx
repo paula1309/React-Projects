c3beb0eaa5a61ce8a0b4a41d7bbc73c06cdfecb3
paula1309 <paulaalexandra1903@hotmail.com>
2022-05-11 21:27:37 -0300

    pagination layout

--- dsmovie/frontend/src/pages/Listing/index.tsx
+++ dsmovie/frontend/src/pages/Listing/index.tsx
@@ -1,8 +1,10 @@
+import Pagination from "components/pagination";
 import React from "react";
+import './styles.css';
 
 function Listing() {
     return (
-        <h1> Página de Listing </h1>
+        <Pagination />
     )
 }
 
